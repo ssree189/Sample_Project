7d9a84adca7e9455d12addc0748c0113eca6b074
Sreekanth Ayyavari <ASreekanth@mwebware.local>
2017-02-15 15:54:24 +0530

    app inti checkin

---
+++ app/Component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-first-angular2-app',
+  templateUrl: 'template.html'
+})
+export class AppComponent { }
---
+++ app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var Module_1 = require('./Module');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(Module_1.AppModule);
+//# sourceMappingURL=main.js.map
